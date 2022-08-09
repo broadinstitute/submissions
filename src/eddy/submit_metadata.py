e7e580d59051441792f2c858b9566979faa3ad03
phendriksen100 <103142505+phendriksen100@users.noreply.github.com>
2022-08-09 15:59:41 -0400

    adding json to submit to gdc

--- src/eddy/submit_metadata.py
+++ src/eddy/submit_metadata.py
@@ -10,11 +10,11 @@ def submit(input):
     data = json.load(file)
     url = f"{endpoint}/{input['program']}/{input['project']}"
 
-    # print("Submit METADATA to GDC dry_run endpoint for PROGRAM PROJECT.")
+    print("Submit METADATA to GDC dry_run endpoint for PROGRAM PROJECT.")
     try:
+        linkSampleAndAliqout(url, input)
         # getGdcShemas()
-        print("before call url", f'{url}/_dry_run')
-        dryRunResponse = requests.post(f'{url}/_dry_run',
+        dryRunResponse = requests.put(f'{url}/_dry_run',
             data = json.dumps(data),
             headers = getHeaders(input)
         )
@@ -24,7 +24,7 @@ def submit(input):
         print("response", dryRunResponse)
         transaction_id = dryRunResponse['transaction_id']
 
-        if dryRunResponse['success'] == true:
+        if dryRunResponse['success'] == True:
             print("Successfully submitted metadata for transaction", transaction_id)
             operation = "commit"
         else:
@@ -43,10 +43,44 @@ def getEntity(queryType, program, project, submitterId, token):
     if queryType == "sar":
         query = """query submitted_aligned_reads ($project_id: String, $submitter_id: Int) { id }"""
     else:
-        query = """query aliquot(project_id: String, submitter_id: String) { id }"""
+        fields = [
+            "id"
+        ]
+        fields = ",".join(fields)
+
+        filters = {
+            "op": "and",
+            "content": [
+                {
+                    "op": "in",
+                    "content":{
+                        "field": "project_id",
+                        "value": [f"{program}-{project}"]
+                    }
+                },
+                {
+                    "op": "in",
+                    "content":{
+                        "field": "submitter_id",
+                        "value": [submitterId]
+                    }
+                }
+            ]
+        }
+
+        params = {
+            "filters": json.dumps(filters),
+            "fields": fields,
+            "format": "JSON",
+            "size": "1000"
+        }
+
+        response = requests.post(f"{endpoint}/{program}/{project}", headers = {"Content-Type": "application/json"}, json = params)
+
+        print("this is the graphql response", response.text)
 
     variables = {
-        'project_id': f'{program}-{project}',
+        'type': 'case',
         'submitter_id': submitterId
     }
     print(query)
@@ -64,6 +98,57 @@ def getEntity(queryType, program, project, submitterId, token):
         }
     )
 
+def linkSampleAndAliqout(url, input):
+    data = [
+        {
+            "type": "case",
+            "projects": {
+                "code": "TEST4"
+            },
+            "submitter_id": "BROAD-TEST4-0001"
+        },
+        {
+            "type": "sample",
+            "cases": {
+                "submitter_id": "BROAD-TEST4-0001"
+            },
+            "submitter_id": "Test_sample_1",
+            "sample_type": "Primary Tumor",
+            "tissue_type": "Tumor"
+        },
+        {
+            "type": "aliquot",
+            "samples": {
+                "submitter_id": "Test_sample_1"
+            },
+            "submitter_id": "Test_aliquot_1"
+        },
+        {
+            "type": "read_group",
+            "aliquots": {
+                "submitter_id": "Test_aliquot_1"
+            },
+            "submitter_id": "Test_read_group_1",
+            "experiment_name": "This is a test",
+            "sequencing_center": "TEST",
+            "platform": "Other",
+            "library_selection": "Other",
+            "library_strategy": "WGS",
+            "library_name": "TEST",
+            "is_paired_end": True,
+            "read_length": 151,
+            "read_group_name": "TEST",
+            "target_capture_kit": "Unknown"
+        }
+    ]
+
+    response = requests.put(url,
+        data = json.dumps(data),
+        headers = getHeaders(input)
+    )
+
+    print("this is the response of creating the links", response.text)
+    
 def getHeaders(input):
     # fileToken = open('tokens/gdc-token.txt')
     # token = fileToken.read()
