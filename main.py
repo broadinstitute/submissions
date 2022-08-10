c8a2aeed58ce92b488be3af3b8b94cce1aa1832a
phendriksen100 <103142505+phendriksen100@users.noreply.github.com>
2022-08-10 13:59:14 -0400

    writes to file

--- main.py
+++ main.py
@@ -6,24 +6,35 @@ from src import access
 from src import submit, getEntity
 import time
 
+#### TODO - Need to add BETTER error handling for the whole application ### 
+
 def main(argv):
     inputData = getCommandLineInput(argv)
-    # This needs to go in its own workflow - verifyRegistration(inputData)
+    
+    # if we have agg_project in the input we know this is a submit workflow
     if "agg_project" in inputData:
         submit(inputData)
 
-        # now lets sleep() for a little to ensure that gdc has finished  
+        # now lets sleep() for a little to ensure that gdc has finished updating the UUID
         time.sleep(10)
 
-        # now lets grab the submitted-aligned-reads-id
-        submitterId = f"{inputData['alias']}.{inputData['data_type']}.{inputData['agg_project']}"
+        # now lets grab the submitted-aligned-reads-id *Need to change this back after testing
+        # submitterId = f"{inputData['alias']}.{inputData['data_type']}.{inputData['agg_project']}"
+        submitterId = "Test_aligned_1"
         sarId = getEntity("sar", inputData['program'], inputData['project'], submitterId, inputData['token'])
-        print(sarId)
+        f = open("UUID.txt", w)
+        f.write(sarId)
+        f.close()
     else:
         print("Verify Registration")
         isValid = verifyRegistration(inputData)
-        print("isvalid", isValid)
 
+        if isValid:
+            print("Sample is valid in GDC")
+        else:
+            raise RuntimeError("Sample is not registered in GDC")
+
+# Parses the command line input and populates the input dictionary
 def getCommandLineInput(argv):
     inputData = {}
     longOptions = [
@@ -41,11 +52,11 @@ def getCommandLineInput(argv):
 
     return inputData
 
+# Calls GDC to check if the sample is registered
 def verifyRegistration(inputData):
     response = getEntity("verify", inputData['program'], inputData['project'], inputData['alias'], inputData['token'])
     response = json.loads(response.text)
 
-    print("response", response)
     if response['data'] and response['data']['aliquot'] and len(response['data']['aliquot']) > 0:
         return True
     else:
