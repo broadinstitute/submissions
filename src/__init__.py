efb03255020ce4b64825e7052507d696134a095d
phendriksen100 <103142505+phendriksen100@users.noreply.github.com>
2022-08-29 15:02:32 -0400

    finsihing validate status

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,2 @@
 from src.eddy.begin import access
-from src.eddy.submit_metadata import submit
-from src.eddy.submit_metadata import getEntity
+from src.eddy.submit_metadata import submit, getEntity, readMetadata
