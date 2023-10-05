8ad84f2e92366180ad3d32d2a53f58cacacb05a3
Nareh Sahakian <sahakian@broadinstitute.org>
2023-10-05 15:49:10 -0400

    wip

--- src/scripts/ega_utils.py
+++ src/scripts/ega_utils.py
@@ -1,91 +1,138 @@
-EGA_LIBRARY_LAYOUT_TYPES = ["SINGLE", "PAIRED"]
+import dataclasses
+from enum import Enum
 
-EGA_LIBRARY_STRATEGY_TYPES = [
-    "WGS",
-    "WGA",
-    "WXS",
-    "RNA-Seq",
-    "ssRNA-seq",
-    "miRNA-Seq",
-    "ncRNA-Seq",
-    "FL-cDNA",
-    "EST",
-    "Hi-C",
-    "ATAC-seq",
-    "WCS",
-    "RAD-Seq",
-    "CLONE",
-    "POOLCLONE",
-    "AMPLICON",
-    "CLONEEND",
-    "FINISHING",
-    "ChIP-Seq",
-    "MNase-Seq",
-    "DNase-Hypersensitivity",
-    "Bisulfite-Seq",
-    "CTS",
-    "MRE-Seq",
-    "MeDIP-Seq",
-    "MBD-Seq",
-    "Tn-Seq",
-    "VALIDATION",
-    "FAIRE-seq",
-    "SELEX",
-    "RIP-Seq",
-    "ChIA-PET",
-    "Synthetic-Long-Read",
-    "Targeted-Capture",
-    "Tethered Chromatin Conformation Capture",
-    "NOMe-Seq",
-    "ChM-Seq",
-    "GBS",
-    "OTHER",
-    "snRNA-seq",
-    "Ribo-Seq",
-]
 
-EGA_LIBRARY_SOURCE_TYPES = [
-    "GENOMIC",
-    "GENOMIC SINGLE CELL",
-    "TRANSCRIPTOMIC",
-    "TRANSCRIPTOMIC SINGLE CELL",
-    "METAGENOMIC",
-    "METATRANSCRIPTOMIC",
-    "SYNTHETIC",
-    "VIRAL RNA",
-    "OTHER"
-]
+class LibraryLayouts(str, Enum):
+    SINGLE = "SINGLE"
+    PAIRED = "PAIRED"
 
-EGA_LIBRARY_SELECTION_TYPES = [
-    "RANDOM",
-    "PCR",
-    "RANDOM PCR",
-    "RT-PCR",
-    "HMPR",
-    "MF",
-    "repeat fractionation",
-    "size fractionation",
-    "MSLL",
-    "cDNA",
-    "cDNA_randomPriming",
-    "cDNA_oligo_dT",
-    "PolyA",
-    "Oligo-dT",
-    "Inverse rRNA",
-    "Inverse rRNA selection",
-    "ChIP",
-    "ChIP-Seq",
-    "MNase",
-    "DNase",
-    "Hybrid Selection",
-    "Reduced Representation",
-    "Restriction Digest",
-    "5-methylcytidine antibody",
-    "MBD2 protein methyl-CpG binding domain",
-    "CAGE",
-    "RACE",
-    "MDA",
-    "padlock probes capture method",
-    "other",
-    "unspecified"
-]
+
+class LibraryStrategy(str, Enum):
+    WGS = "WGS"
+    WGA = "WGA"
+    WXS = "WXS"
+    RNA_SEQ = "RNA-Seq"
+    SSRNA_SEQ = "ssRNA-seq"
+    MIRNA_SEQ = "miRNA-Seq"
+    NCRNA_SEQ = "ncRNA-Seq"
+    FL_CDNA = "FL-cDNA"
+    EST = "EST"
+    Hi_C = "Hi-C"
+    ATAC_SEQ = "ATAC-seq"
+    WCS = "WCS"
+    RAD_SEQ = "RAD-Seq"
+    CLONE = "CLONE"
+    POOLCLONE = "POOLCLONE"
+    AMPLICON = "AMPLICON"
+    CLONEEND = "CLONEEND"
+    FINISHING = "FINISHING"
+    CHIP_SEQ = "ChIP-Seq"
+    MNASE_Seq = "MNase-Seq"
+    DNASE_HYPERSENSITIVITY = "DNase-Hypersensitivity"
+    BISULFATE_SEQ = "Bisulfite-Seq"
+    CTS = "CTS"
+    MRE_SEQ = "MRE-Seq"
+    MEDIP_SEQ = "MeDIP-Seq"
+    MBD_SEQ = "MBD-Seq"
+    TN_SEQ = "Tn-Seq"
+    VALIDATION = "VALIDATION"
+    FAIRE_SEQ = "FAIRE-seq"
+    SELEX = "SELEX"
+    RIP_SEQ = "RIP-Seq"
+    CHIA_PET = "ChIA-PET"
+    SYNTHETIC_LONG_READ = "Synthetic-Long-Read"
+    TARGETED_CAPTURE = "Targeted-Capture"
+    TETHERED_CHROMATIN_CONFORMATION_CAPTURE = "Tethered Chromatin Conformation Capture"
+    NOME_SEQ = "NOMe-Seq"
+    CHM_SEQ = "ChM-Seq"
+    GBS = "GBS"
+    OTHER = "OTHER"
+    SNRNA_SEQ = "snRNA-seq"
+    RIBO_SEQ = "Ribo-Seq"
+
+
+class LibrarySource(str, Enum):
+    GENOMIC = "GENOMIC"
+    GENOMIC_SINGLE_CELL = "GENOMIC SINGLE CELL"
+    TRANSCRIPTOMIC = "TRANSCRIPTOMIC"
+    TRANSCRIPTOMIC_SINGLE_CELL = "TRANSCRIPTOMIC SINGLE CELL"
+    METAGENOMIC = "METAGENOMIC"
+    METATRANSCRIPTOMIC = "METATRANSCRIPTOMIC"
+    SYNTHETIC = "SYNTHETIC"
+    VIRAL_RNA = "VIRAL RNA"
+    OTHER = "OTHER"
+
+
+class LibrarySelection(str, Enum):
+    RANDOM = "RANDOM"
+    PCR = "PCR"
+    RANDOM_PCR = "RANDOM PCR"
+    RT_PCR = "RT-PCR"
+    HMPR = "HMPR"
+    MF = "MF"
+    REPEAT_FRACTIONATION = "repeat fractionation"
+    SIZE_FRACTIONATION = "size fractionation"
+    MSLL = "MSLL"
+    CDNA = "cDNA"
+    CDNA_RANDOMPRIMING = "cDNA_randomPriming"
+    CDNA_OLIGO_DT = "cDNA_oligo_dT"
+    POLYA = "PolyA"
+    OLIGO_DT = "Oligo-dT"
+    INVERSE_RRNA = "Inverse rRNA"
+    INVERSE_RRNA_ELECTION = "Inverse rRNA selection"
+    CHIP = "ChIP"
+    CHIP_SEQ = "ChIP-Seq"
+    MNASE = "MNase"
+    DNASE = "DNase"
+    HYBRID_SELECTION = "Hybrid Selection"
+    REDUCED_REPRESENTATION = "Reduced Representation"
+    RESTRICTION_DIGEST = "Restriction Digest",
+    FIVE_METHYLCYTIDINE_ANTIBODY =  "5-methylcytidine antibody"
+    MBD2_PROTEIN_METHYL_CPG_BINDING_DOMAIN = "MBD2 protein methyl-CpG binding domain"
+    CAGE = "CAGE"
+    RACE = "RACE"
+    MDA = "MDA"
+    PADLOCK_PROBES_CAPTURE_METHOD = "padlock probes capture method"
+    OTHER = "other"
+    UNSPECIFIED = "unspecified"
+
+
+class RunFileType(str, Enum):
+    SRF = "srf"
+    SFF = "sff"
+    FASTQ = "fastq"
+    ILLUMINA_NATIVE = "Illumina_native"
+    ILLUMINA_NATIVE_GSEQ = "Illumina_native_qseq"
+    SOLID_NATIVE_CSFASTA = "SOLiD_native_csfasta"
+    PACBIO_HDF5 = "PacBio_HDF5"
+    BAM = "bam"
+    CRAM = "cram"
+    COMPLETEGENOMICS_NATIVE = "CompleteGenomics_native"
+    OXFORDNANOPORE_NATIVE = "OxfordNanopore_native"
+
+
+INSTRUMENT_MODEL_MAPPING = {
+    "HiSeq X Five": 8,
+    "HiSeq X Ten": 9,
+    "Illumina Genome Analyzer": 10,
+    "Illumina Genome Analyzer II": 11,
+    "Illumina Genome Analyzer IIx": 12,
+    "Illumina HiScanSQ": 13,
+    "Illumina HiSeq 1000": 14,
+    "Illumina HiSeq 1500": 15,
+    "Illumina HiSeq 2000": 16,
+    "Illumina HiSeq 2500": 17,
+    "Illumina HiSeq 3000": 18,
+    "Illumina HiSeq 4000": 19,
+    "Illumina HiSeq X": 20,
+    "Illumina iSeq 100": 21,
+    "Illumina MiSeq": 22,
+    "Illumina MiniSeq": 23,
+    "Illumina NovaSeq X": 24,
+    "Illumina NovaSeq 6000": 25,
+    "NextSeq 500": 26,
+    "NextSeq 550": 27,
+    "NextSeq 1000": 28,
+    "NextSeq 2000": 29,
+    "unspecified": 30,
+}
--- src/scripts/upload_ega_metadata.py
+++ src/scripts/upload_ega_metadata.py
@@ -13,21 +13,23 @@
         https://ega-archive.org/submission/metadata/submission/sequencing-phenotype/submitter-portal-api/
     - Submission API Documentation: https://submission.ega-archive.org/api/spec/#/
 """
-
+import sys
 import argparse
 import requests
 from typing import Optional
 
+sys.path.append("./")
 from src.scripts.ega_utils import (
-    EGA_LIBRARY_STRATEGY_TYPES,
-    EGA_LIBRARY_SOURCE_TYPES,
-    EGA_LIBRARY_SELECTION_TYPES,
-    EGA_LIBRARY_LAYOUT_TYPES
+    LibraryLayouts,
+    LibraryStrategy,
+    LibrarySource,
+    LibrarySelection,
+    RunFileType, INSTRUMENT_MODEL_MAPPING,
 )
 
 
 IDP_URL = "https://idp.ega-archive.org/realms/EGA/protocol/openid-connect/token"
-SUBMISSION_PROTOCOL_URL = "https://submission.ega-archive.org/api"
+SUBMISSION_PROTOCOL_API_URL = "https://submission.ega-archive.org/api"
 
 
 class LoginAndGetToken:
@@ -59,16 +61,20 @@ class CreateExperimentAndRuns:
     # TODO: figure out if the study_provisional_id is required if the study already has an accession id
     # TODO: figure out if the extra_attributes are always required, or if the tag/value only required if passing
     #  extra args
+    VALID_STATUS_CODES = [200, 201]
+
     def __init__(
-            self, token: str,
+            self,
+            token: str,
             submission_accession_id: str,
             study_accession_id: str,
             design_description: str,
             instrument_model_id: int,
-            library_layout: str,
-            library_strategy: str,
-            library_source: str,
-            library_selection: str,
+            library_layout: LibraryLayouts,
+            library_strategy: LibraryStrategy,
+            library_source: LibrarySource,
+            library_selection: LibrarySelection,
+            run_file_type: RunFileType,
     ):
         self.token = token
         self.submission_accession_id = submission_accession_id
@@ -79,13 +85,24 @@ class CreateExperimentAndRuns:
         self.library_strategy = library_strategy
         self.library_source = library_source
         self.library_selection = library_selection
+        self.run_file_type = run_file_type
 
-    # TODO: Does this only need to be run once for the experiment, and not once for the sample? Or once per sample?
+    def _headers(self) -> dict:
+        return {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.token}"
+        }
+
     def _create_experiment(self) -> Optional[int]:
-        """Registers the sample experiment"""
+        """
+        Registers the sample experiment
+        Endpoint documentation located here:
+        https://submission.ega-archive.org/api/spec/#/paths/submissions-accession_id--experiments/post
+        """
         response = requests.post(
-            url=f"{SUBMISSION_PROTOCOL_URL}/submissions/{self.submission_accession_id}/experiments",
-            data={
+            url=f"{SUBMISSION_PROTOCOL_API_URL}/submissions/{self.submission_accession_id}/experiments",
+            headers=self._headers(),
+            json={
                 "design_description": self.design_description,
                 "instrument_model_id": self.instrument_model_id,
                 "library_layout": self.library_layout,
@@ -95,7 +112,7 @@ class CreateExperimentAndRuns:
                 "study_accession_id": self.study_accession_id
             }
         )
-        if response.status_code == 200:
+        if response.status_code in self.VALID_STATUS_CODES:
             experiment_id = response.json()["provisional_id"]
             return experiment_id
         raise Exception(
@@ -103,18 +120,49 @@ class CreateExperimentAndRuns:
             f"register experiment"
         )
 
-    # TODO: figure out how to get a sample's accession id
-    def _get_sample_accession_id(self):
-        """Retrieves the sample accession ID"""
-        pass
+    def _get_sample_accession_id(self) -> list[int]:
+        """
+        Retrieves the sample accession ID(s)
+        Endpoint documentation located here:
+        https://submission.ega-archive.org/api/spec/#/paths/files/get
+        """
+        response = requests.get(
+            url=f"{SUBMISSION_PROTOCOL_API_URL}/files",
+            headers=self._headers(),
 
-    def _create_run(self):
-        """Registers the run for the sample"""
+        )
 
+    def _create_run(self, experiment_provisional_id: int, sample_accession_ids: list[int]) -> Optional[list]:
+        """
+        Registers the run for the sample
+        Endpoint documentation located here:
+        https://submission.ega-archive.org/api/spec/#/paths/submissions-accession_id--runs/post
+        """
+        response = requests.post(
+            url=f"{SUBMISSION_PROTOCOL_API_URL}/submissions/{self.submission_accession_id}/runs",
+            headers=self._headers(),
+            json={
+                "run_file_type": self.run_file_type,
+                "files": sample_accession_ids,
+                "experiment_provisional_id": experiment_provisional_id,
+            }
+        )
+        if response.status_code in self.VALID_STATUS_CODES:
+            run_accession_ids = [a["provisional_id"] for a in response.json()]
+            return run_accession_ids
+        raise Exception(
+            f"Received status code {response.status_code} with error: {response.json()} while attempting to "
+            f"register experiment"
+        )
 
     def run(self):
-        experiment_id = self._create_experiment()
-        self._get_sample_accession_id()
+        experiment_provisional_id = self._create_experiment()
+        sample_accession_id = self._get_sample_accession_id()
+        run_accesion_ids = self._create_run(
+            experiment_provisional_id=experiment_provisional_id,
+            sample_accession_ids=sample_accession_id
+        )
+        print(run_accesion_ids)
 
 
 if __name__ == "__main__":
@@ -146,59 +194,60 @@ if __name__ == "__main__":
         required=True,
         help="The design description"
     )
-    # TODO: add a mapping to pull the correct ID based on these pairings: https://github.com/EllenElizabethWinchester/ega-submission/blob/main/Resources/platform_models.json
     parser.add_argument(
         "-instrument_model_id",
         required=True,
-        help="The experiment instrument model ID"
+        help="The experiment instrument model ID",
+        choices=INSTRUMENT_MODEL_MAPPING.keys()
     )
     # Source: https://github.com/EllenElizabethWinchester/ega-submission/blob/main/Resources/library_layouts.json
     parser.add_argument(
         "-library_layout",
         required=True,
         help="The experiment library layout",
-        choices=EGA_LIBRARY_LAYOUT_TYPES
+        choices=list(LibraryLayouts)
     )
     # Source: https://github.com/EllenElizabethWinchester/ega-submission/blob/main/Resources/library_strategies.json
     parser.add_argument(
         "-library_strategy",
         required=True,
         help="The experiment library strategy.",
-        choices=EGA_LIBRARY_STRATEGY_TYPES
+        choices=list(LibraryStrategy)
     )
     # Source: https://github.com/EllenElizabethWinchester/ega-submission/blob/main/Resources/library_sources.json
     parser.add_argument(
         "-library_source",
         required=True,
         help="The experiment library source",
-        choices=EGA_LIBRARY_SOURCE_TYPES
+        choices=list(LibrarySource)
     )
     # Source: https://github.com/EllenElizabethWinchester/ega-submission/blob/main/Resources/library_selections.json
     parser.add_argument(
         "-library_selection",
         required=True,
         help="The experiment library selection",
-        choices=EGA_LIBRARY_SELECTION_TYPES
+        choices=list(LibrarySelection)
     )
-    # TODO: add a mapping to
+    # Source: https://github.com/EllenElizabethWinchester/ega-submission/blob/main/Resources/run_file_types.json
     parser.add_argument(
         "-run_file_type",
         required=True,
         help="The run file type.",
-        choices=
+        choices=list(RunFileType)
     )
 
-
     args = parser.parse_args()
+
     access_token = LoginAndGetToken(username=args.username, password=args.password).login_and_get_token()
     CreateExperimentAndRuns(
         token=access_token,
         submission_accession_id=args.submission_accession_id,
         study_accession_id=args.study_accession_id,
         design_description=args.design_description,
-        instrument_model_id=args.instrument_model_id,
+        instrument_model_id=INSTRUMENT_MODEL_MAPPING[args.instrument_model_id],
         library_layout=args.library_layout,
         library_strategy=args.library_strategy,
         library_source=args.library_source,
-        library_selection=args.library_selection
+        library_selection=args.library_selection,
+        run_file_type=args.run_file_type,
     ).run()
