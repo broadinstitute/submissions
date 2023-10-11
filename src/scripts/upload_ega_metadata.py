3388208e54327b0bd159f0e715eed0451d96b7e4
Nareh Sahakian <sahakian@broadinstitute.org>
2023-10-11 17:35:00 -0400

    add more logging, fix how responses are used based on returns types

--- src/scripts/upload_ega_metadata.py
+++ src/scripts/upload_ega_metadata.py
@@ -15,10 +15,12 @@
     - Submission API Documentation:
         https://submission.ega-archive.org/api/spec/#/
 """
+import logging
 import sys
 import argparse
 import requests
 from typing import Optional
+from datetime import datetime
 
 sys.path.append("./")
 from src.scripts.ega_utils import (
@@ -31,7 +33,7 @@ from src.scripts.ega_utils import (
 )
 
 
-IDP_URL = "https://idp.ega-archive.org/realms/EGA/protocol/openid-connect/token"
+LOGIN_URL = "https://idp.ega-archive.org/realms/EGA/protocol/openid-connect/token"
 SUBMISSION_PROTOCOL_API_URL = "https://submission.ega-archive.org/api"
 
 
@@ -40,11 +42,11 @@ class LoginAndGetToken:
         self.username = username
         self.password = password
 
-    def login_and_get_token(self) -> str:
+    def login_and_get_token(self) -> Optional[str]:
         """Logs in and retrieves access token"""
-        response = requests.get(
-            url=IDP_URL,
-            params={
+        response = requests.post(
+            url=LOGIN_URL,
+            data={
                 "grant_type": "password",
                 "client_id": "sp-api",
                 "username": self.username,
@@ -53,17 +55,16 @@ class LoginAndGetToken:
         )
         if response.status_code == 200:
             token = response.json()["access_token"]
+            logging.info("Successfully created access token!")
             return token
-        raise Exception(
-            f"Received status code {response.status_code} with error {response.json()} while attempting to "
-            f"get access token"
-        )
+        else:
+            error_message = f"""Received status code {response.status_code} with error {response.json()} while 
+            attempting to get access token"""
+            logging.error(error_message)
+            raise Exception(error_message)
 
 
 class RegisterEgaMetadata:
-    # TODO: figure out if the study_provisional_id is required if the study already has an accession id
-    # TODO: figure out if the extra_attributes are always required, or if the tag/value only required if passing
-    #  extra args
     VALID_STATUS_CODES = [200, 201]
 
     def __init__(
@@ -71,24 +72,24 @@ class RegisterEgaMetadata:
             token: str,
             submission_accession_id: str,
             study_accession_id: str,
-            design_description: str,
             instrument_model_id: int,
             library_layout: LibraryLayouts,
             library_strategy: LibraryStrategy,
             library_source: LibrarySource,
             library_selection: LibrarySelection,
             run_file_type: RunFileType,
+            technology: str,
     ):
         self.token = token
         self.submission_accession_id = submission_accession_id
         self.study_accession_id = study_accession_id
-        self.design_description = design_description
         self.instrument_model_id = instrument_model_id
         self.library_layout = library_layout
         self.library_strategy = library_strategy
         self.library_source = library_source
         self.library_selection = library_selection
         self.run_file_type = run_file_type
+        self.technology = technology
 
     def _headers(self) -> dict:
         return {
@@ -98,15 +99,17 @@ class RegisterEgaMetadata:
 
     def _create_experiment(self) -> Optional[int]:
         """
-        Registers the sample experiment
+        Registers the "experiment"
         Endpoint documentation located here:
         https://submission.ega-archive.org/api/spec/#/paths/submissions-accession_id--experiments/post
         """
+        design_description = f"{self.technology} sequencing of Homo sapiens via {self.library_selection}"
+
         response = requests.post(
             url=f"{SUBMISSION_PROTOCOL_API_URL}/submissions/{self.submission_accession_id}/experiments",
             headers=self._headers(),
-            json={
-                "design_description": self.design_description,
+            data={
+                "design_description": design_description,
                 "instrument_model_id": self.instrument_model_id,
                 "library_layout": self.library_layout,
                 "library_strategy": self.library_strategy,
@@ -116,12 +119,14 @@ class RegisterEgaMetadata:
             }
         )
         if response.status_code in self.VALID_STATUS_CODES:
-            experiment_id = response.json()["provisional_id"]
-            return experiment_id
-        raise Exception(
-            f"Received status code {response.status_code} with error: {response.json()} while attempting to "
-            f"register experiment"
-        )
+            experiment_provisional_id = response.json()["provisional_id"]
+            logging.info(f"Successfully created experiment with id: {experiment_provisional_id}")
+            return experiment_provisional_id
+        else:
+            error_message = f"""Received status code {response.status_code} with error: {response.json()} while 
+            attempting to register experiment"""
+            logging.error(error_message)
+            raise Exception(error_message)
 
     def _get_sample_accession_id(self) -> list[int]:
         """
@@ -130,7 +135,9 @@ class RegisterEgaMetadata:
         https://submission.ega-archive.org/api/spec/#/paths/files/get
         """
         # TODO: we need the file format so we know the correct prefix in order to filter on files that correspond to
-        #  the one sample
+        #  the one sample (assuming we can filter by the sample_alias here?)
+        # NOTE: This should somehow filter for the files that are part of this "submission" so that this only has to
+        # be run once
         response = requests.get(
             url=f"{SUBMISSION_PROTOCOL_API_URL}/files",
             headers=self._headers(),
@@ -141,11 +148,12 @@ class RegisterEgaMetadata:
         )
         if response.status_code in self.VALID_STATUS_CODES:
             file_provisional_ids = [a["provisional_id"] for a in response.json()]
+            logging.info("Successfully retrieved files")
             return file_provisional_ids
-        raise Exception(
-            f"Received status code {response.status_code} with error: {response.json()} while attempting to "
-            f"get sample accession ids"
-        )
+        else:
+            error_message = f"""Received status code {response.status_code} with error: {response.json()} while
+             attempting to get sample accession ids"""
+            raise Exception(error_message)
 
     def _create_run(self, experiment_provisional_id: int, sample_accession_ids: list[int]) -> Optional[list]:
         """
@@ -156,7 +164,7 @@ class RegisterEgaMetadata:
         response = requests.post(
             url=f"{SUBMISSION_PROTOCOL_API_URL}/submissions/{self.submission_accession_id}/runs",
             headers=self._headers(),
-            json={
+            data={
                 "run_file_type": self.run_file_type,
                 "files": sample_accession_ids,
                 "experiment_provisional_id": experiment_provisional_id,
@@ -164,11 +172,12 @@ class RegisterEgaMetadata:
         )
         if response.status_code in self.VALID_STATUS_CODES:
             run_provisional_ids = [a["provisional_id"] for a in response.json()]
+            logging.info("Successfully registered runs")
             return run_provisional_ids
-        raise Exception(
-            f"Received status code {response.status_code} with error: {response.json()} while attempting to "
-            f"register run"
-        )
+        else:
+            error_message = f"""Received status code {response.status_code} with error: {response.json()} while 
+            attempting to register run"""
+            raise Exception(error_message)
 
     def _get_policy_accession_id(self) -> Optional[str]:
         """
@@ -183,17 +192,20 @@ class RegisterEgaMetadata:
         )
         if response.status_code in self.VALID_STATUS_CODES:
             policy_accession_id = [a["accession_id"] for a in response.json()]
+            # TODO This logic needs to be updated based on what the PM provides for us
             if len(policy_accession_id) > 1:
                 raise ValueError(
                     f"Expected to find one policy associated with inbox, but found {len(policy_accession_id)}"
                 )
+            logging.info("Successfully retrieved policy DAC")
             return policy_accession_id[0]
-        raise Exception(
-            f"Received status code {response.status_code} with error: {response.json()} while attempting to "
-            f"get policy accession ID"
-        )
+        else:
+            error_message = f"""Received status code {response.status_code} with error: {response.json()} 
+            while attempting to get policy accession ID"""
+            logging.error(error_message)
+            raise Exception(error_message)
 
-    def _create_dataset(self, policy_accession_id: str, run_provisional_ids: list[int]):
+    def _create_dataset(self, policy_accession_id: str, run_provisional_ids: list[int]) -> Optional[int]:
         """
         Registers the dataset of runs
         Endpoint documentation located here:
@@ -202,39 +214,77 @@ class RegisterEgaMetadata:
 
         # TODO: determine what the title should be here
         # TODO: determine what the description should be here
-        # TODO: determine what the enums are for the dataset_types param
-        # TODO: do we need to get the run accession ids?
+        wgs = "Whole genome sequencing"
+        exome = "Exome sequencing"
+        dataset_type = wgs if self.library_strategy == LibraryStrategy.WGS else exome
 
         response = requests.post(
             url=f"{SUBMISSION_PROTOCOL_API_URL}/submissions/{self.submission_accession_id}/datasets",
             headers=self._headers(),
-            json={
+            data={
                 "title": "",
                 "description": "",
-                "dataset_types": [""],
+                "dataset_types": [dataset_type],
                 "policy_accession_id": policy_accession_id,
                 "run_provisional_ids": run_provisional_ids,
             }
         )
         if response.status_code in self.VALID_STATUS_CODES:
-            dataset_provisional_id = response.json()["provisional_id"]
+            dataset_provisional_id = [r["provisional_id"] for r in response.json()][0]
+            logging.info("Successfully registered dataset!")
             return dataset_provisional_id
-        raise Exception(
-            f"Received status code {response.status_code} with error: {response.json()} while attempting to "
-            f"register dataset"
-        )
+        else:
+            error_message = f"""Received status code {response.status_code} with error: {response.json()} while 
+            attempting to register dataset"""
+            logging.error(error_message)
+            raise Exception(error_message)
 
-    def run(self):
-        experiment_provisional_id = self._create_experiment()
-        sample_accession_id = self._get_sample_accession_id()
-        run_provisional_ids = self._create_run(
-            experiment_provisional_id=experiment_provisional_id,
-            sample_accession_ids=sample_accession_id
+    def _finalize_submission(self) -> None:
+        """
+        Finalizes the submission
+        Endpoint documentation located here:
+        https://submission.ega-archive.org/api/spec/#/paths/submissions-accession_id--finalise/post
+        """
+        timestamp = datetime.now().strftime("%Y-%m-%d")
+        response = requests.post(
+            url=f"{SUBMISSION_PROTOCOL_API_URL}/submissions/{self.submission_accession_id}/finalise",
+            headers=self._headers(),
+            data={
+                "expected_release_date": timestamp,
+            }
         )
-        policy_accession_id = self._get_policy_accession_id()
-        self._create_dataset(policy_accession_id=policy_accession_id, run_provisional_ids=run_provisional_ids)
-        # TODO: determine if the finalize step is necessary?? Don't see the endpoint but listed as a
-        #  step in the overview
+        if response.status_code in self.VALID_STATUS_CODES:
+            logging.info(
+                f"Successfully finalized submission for submission accession id: {self.submission_accession_id}"
+            )
+        else:
+            error_message = f"""Received status code {response.status_code} with error {response.json()} while
+            attempting to finalize submission"""
+            logging.error(error_message)
+            raise Exception(error_message)
+
+    def register_metadata(self):
+        # Register the experiment
+        experiment_provisional_id = self._create_experiment()
+        # Gather sample accession IDs
+        sample_accession_id = self._get_sample_accession_id()
+        # If both successful, create the runs
+        if experiment_provisional_id and sample_accession_id:
+            run_provisional_ids = self._create_run(
+                experiment_provisional_id=experiment_provisional_id,
+                sample_accession_ids=sample_accession_id
+            )
+            # If creating the runs is successful, gather thea appropriate DAC policy info
+            policy_accession_id = self._get_policy_accession_id()
+            # If the DAC policy is found, create the dataset
+            if policy_accession_id and run_provisional_ids:
+                dataset_provisional_id = self._create_dataset(
+                    policy_accession_id=policy_accession_id,
+                    run_provisional_ids=run_provisional_ids
+                )
+                # If the dataset is created, finalize the submission
+                if dataset_provisional_id:
+                    self._finalize_submission()
 
 
 if __name__ == "__main__":
@@ -262,64 +312,66 @@ if __name__ == "__main__":
         help="The EGA password"
     )
     parser.add_argument(
-        "-design_description",
+        "-instrument_model",
         required=True,
-        help="The design description"
+        help="The experiment instrument model",
+        choices=INSTRUMENT_MODEL_MAPPING.values()
     )
-    parser.add_argument(
-        "-instrument_model_id",
-        required=True,
-        help="The experiment instrument model ID",
-        choices=INSTRUMENT_MODEL_MAPPING.keys()
-    )
-    # Source: https://github.com/EllenElizabethWinchester/ega-submission/blob/main/Resources/library_layouts.json
+    """Source: https://submission.ega-archive.org/api/spec/#/paths/enums-library_layouts/get"""
     parser.add_argument(
         "-library_layout",
         required=True,
         help="The experiment library layout",
         choices=list(LibraryLayouts)
     )
-    # Source: https://github.com/EllenElizabethWinchester/ega-submission/blob/main/Resources/library_strategies.json
+    """Source: https://submission.ega-archive.org/api/spec/#/paths/enums-library_strategies/get"""
     parser.add_argument(
         "-library_strategy",
         required=True,
         help="The experiment library strategy.",
         choices=list(LibraryStrategy)
     )
-    # Source: https://github.com/EllenElizabethWinchester/ega-submission/blob/main/Resources/library_sources.json
+    """Source: https://submission.ega-archive.org/api/spec/#/paths/enums-library_sources/get"""
     parser.add_argument(
         "-library_source",
         required=True,
         help="The experiment library source",
         choices=list(LibrarySource)
     )
-    # Source: https://github.com/EllenElizabethWinchester/ega-submission/blob/main/Resources/library_selections.json
+    """ Source: https://submission.ega-archive.org/api/spec/#/paths/enums-library_selections/get"""
     parser.add_argument(
         "-library_selection",
         required=True,
         help="The experiment library selection",
         choices=list(LibrarySelection)
     )
-    # Source: https://github.com/EllenElizabethWinchester/ega-submission/blob/main/Resources/run_file_types.json
+    """Source: https://submission.ega-archive.org/api/spec/#/paths/enums-run_file_types/get"""
     parser.add_argument(
         "-run_file_type",
         required=True,
         help="The run file type.",
         choices=list(RunFileType)
     )
+    parser.add_argument(
+        "-technology",
+        required=False,
+        help="The type of sequencer",
+        default="ILLUMINA",
+    )
 
     args = parser.parse_args()
-
+    # TODO: Add DAC info as required param to script input
     access_token = LoginAndGetToken(username=args.username, password=args.password).login_and_get_token()
-    RegisterEgaMetadata(
-        token=access_token,
-        submission_accession_id=args.submission_accession_id,
-        study_accession_id=args.study_accession_id,
-        design_description=args.design_description,
-        instrument_model_id=INSTRUMENT_MODEL_MAPPING[args.instrument_model_id],
-        library_layout=args.library_layout,
-        library_strategy=args.library_strategy,
-        library_source=args.library_source,
-        library_selection=args.library_selection,
-        run_file_type=args.run_file_type,
-    ).run()
+    if access_token:
+        RegisterEgaMetadata(
+            token=access_token,
+            submission_accession_id=args.submission_accession_id,
+            study_accession_id=args.study_accession_id,
+            instrument_model_id=INSTRUMENT_MODEL_MAPPING[args.instrument_model],
+            library_layout=args.library_layout,
+            library_strategy=args.library_strategy,
+            library_source=args.library_source,
+            library_selection=args.library_selection,
+            run_file_type=args.run_file_type,
+            technology=args.technolgy,
+        ).register_metadata()
