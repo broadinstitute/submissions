148fbd0bac1ae0a12ad6222d3533e3ea78588e81
phendriksen100 <103142505+phendriksen100@users.noreply.github.com>
2023-11-28 13:11:45 -0500

    add project

--- src/scripts/ega/transfer_ega_file.py
+++ src/scripts/ega/transfer_ega_file.py
@@ -7,9 +7,10 @@ def transfer_file(encrypted_data_file, ega_inbox):
     SFTP_HOSTNAME = "inbox.ega-archive.org"
     SFTP_PORT = 22
 
+    print("before")
     # Retrieve the secret value from Google Secret Manager
-    secret_res = subprocess.run(['gcloud', 'secrets', 'versions', 'access', 'latest', f'--secret={secret_name}', '--format=get(payload.data)'], capture_output=True, text=True)
-
+    secret_res = subprocess.run(['gcloud', 'secrets', 'versions', 'access', 'latest', '--secret=ega_password', '--format=get(payload.data)', '--project=gdc-submissions'], capture_output=True, text=True)
+    print("after")
     # Check for errors
     if secret_res.returncode != 0:
         raise RuntimeError(secret_res.stderr)
@@ -22,7 +23,7 @@ def transfer_file(encrypted_data_file, ega_inbox):
 
     # Create an SFTP client from the transport
     sftp = paramiko.SFTPClient.from_transport(transport)
-    remote_file = os.path.join(self.REMOTE_PATH, os.path.basename(encrypted_data_file))
+    remote_file = os.path.join(REMOTE_PATH, os.path.basename(encrypted_data_file))
     sftp.put(encrypted_data_file, remote_file)
 
 if __name__ == '__main__':
@@ -40,5 +41,5 @@ if __name__ == '__main__':
         help="Inbox assigned to the current PM"
     )
     args = parser.parse_args()
-
+    print("starting script")
     transfer_file(args.encrypted_data_file, args.ega_inbox)
