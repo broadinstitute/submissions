edea921a64e496e5f5b392351cc994ef0d8add82
phendriksen100 <103142505+phendriksen100@users.noreply.github.com>
2023-11-29 09:16:09 -0500

    add client

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
 FROM python:3.8
 
+# Install the Google Cloud SDK
+RUN apt-get update && \
+    apt-get install -y apt-transport-https ca-certificates gnupg && \
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg && \
+    apt-get update && \
+    apt-get install -y google-cloud-sdk
+
+
 COPY requirements.txt .
 RUN pip3 install -r requirements.txt
 
--- src/scripts/ega/transfer_ega_file.py
+++ src/scripts/ega/transfer_ega_file.py
@@ -1,25 +1,51 @@
 import os
 import subprocess
 import argparse
+from google.cloud import secretmanager
+import google_crc32c
+
+def get_active_account():
+    try:
+        result = subprocess.run(['gcloud', 'auth', 'list', '--filter=status:ACTIVE', '--format=value(account)'], capture_output=True, text=True)
+        if result.returncode == 0:
+            return result.stdout.strip()
+        else:
+            return f"Error: {result.stderr.strip()}"
+    except Exception as e:
+        return f"Exception: {str(e)}"
+
+def get_ega_password_secret():
+    project_id = "gdc-submissions"
+    secret_id = "ega_password"
+    version_id = 1
+
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+    response = client.access_secret_version(request={"name": name})
+
+    # Verify payload checksum.
+    crc32c = google_crc32c.Checksum()
+    crc32c.update(response.payload.data)
+    if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
+        print("Data corruption detected.")
+
+    # Print the secret payload.
+    payload = response.payload.data.decode("UTF-8")
+    print(f"Plaintext: {payload}")
+
+    return payload
 
 def transfer_file(encrypted_data_file, ega_inbox):
     REMOTE_PATH = "/encrypted"
     SFTP_HOSTNAME = "inbox.ega-archive.org"
     SFTP_PORT = 22
 
-    print("before")
+    print("before - current running account", get_active_account())
     # Retrieve the secret value from Google Secret Manager
-    secret_res = subprocess.run(['gcloud', 'secrets', 'versions', 'access', 'latest', '--secret=ega_password', '--format=get(payload.data)', '--project=gdc-submissions'], capture_output=True, text=True)
-    print("after")
-    # Check for errors
-    if secret_res.returncode != 0:
-        raise RuntimeError(secret_res.stderr)
-
-    # Extract the secret value
-    password = res.stdout.strip()
-
+    ega_password = get_ega_password_secret()
+    print("got password")
     transport = paramiko.Transport((SFTP_HOSTNAME, SFTP_PORT))
-    transport.connect(username=ega_inbox, password=password)
+    transport.connect(username=ega_inbox, password=ega_password)
 
     # Create an SFTP client from the transport
     sftp = paramiko.SFTPClient.from_transport(transport)
