3dbe2f4a04699d965adfa56f167796b9aa807871
phendriksen100 <103142505+phendriksen100@users.noreply.github.com>
2023-11-30 16:18:42 -0500

    clean up encryption file

--- src/scripts/ega/encrypt_data_file.py
+++ src/scripts/ega/encrypt_data_file.py
@@ -7,34 +7,40 @@ logging.basicConfig(
     format="%(levelname)s: %(asctime)s : %(message)s", level=logging.INFO
 )
 
+ENCRYPTED_FILE_PREFIX = "encrypted_"
+
 def encrypt_file(aggregation_path, crypt4gh_encryption_key):
-    # Get the filename from the path
+    """
+    Encrypts the given data file using crypt4gh.
+
+    Parameters:
+    - aggregation_path (str): The file to encrypt.
+    - crypt4gh_encryption_key (str): The key supplied by EGA.
+    """
     filename = os.path.basename(aggregation_path)
-    output_file = f'encrypted_{filename}.c4gh'
+    output_file = f"{ENCRYPTED_FILE_PREFIX}{filename}.c4gh"
 
-    # Command to encrypt data file, writes output to encrypted_{base name of original file}.c4gh
     command = f'crypt4gh encrypt --recipient_pk {crypt4gh_encryption_key} < {aggregation_path} > {output_file}'
-    res = subprocess.run(command, capture_output=True, shell=True)
 
-    if res.stderr:
-        raise RuntimeError(res.stderr.decode())
+    try:
+        subprocess.run(command, check=True, capture_output=True, shell=True)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(f"Error encrypting file: {e.stderr.decode()}") from e
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description="This script will encrypt the given data file using crypt4gh"
+        description="Encrypt the given data file using crypt4gh"
     )
     parser.add_argument(
-        "-aggregation_path",
-        required=True,
-        help="The file to encrypt"
+        "--aggregation-path", required=True, help="The file to encrypt"
     )
     parser.add_argument(
-        "-crypt4gh_encryption_key",
-        required=True,
-        help="The key supplied by ega"
+        "--crypt4gh-encryption-key", required=True, help="The key supplied by EGA"
     )
     args = parser.parse_args()
+
     logging.info("Starting script to encrypt data file")
 
     encrypt_file(args.aggregation_path, args.crypt4gh_encryption_key)
+
     logging.info("Script finished")
