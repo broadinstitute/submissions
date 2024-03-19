c7de13a197ea4df322b294054d9b1a9006f03cd2
phendriksen100 <103142505+phendriksen100@users.noreply.github.com>
2024-03-19 15:00:56 -0400

    forgot to save

--- src/services/dbgap_telemetry_report.py
+++ src/services/dbgap_telemetry_report.py
@@ -1,46 +1,49 @@
 import requests
-import json
-import logging
 import xml.etree.ElementTree as ET
 
-logging.basicConfig(
-    format="%(levelname)s: %(asctime)s : %(message)s", level=logging.INFO
-)
+class SampleNotFoundError(Exception):
+    """Exception raised when a sample is not found."""
+    def __init__(self, alias):
+        self.alias = alias
+        super().__init__(f"Sample with alias '{alias}' not found")
 
 class DbgapTelemetryWrapper:
     def __init__(self, phs_id=None):
         self.endpoint = f"https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/GetSampleStatus.cgi?rettype=xml&study_id={phs_id}"
         self.phs_id = phs_id
 
-    def call_telemetry_report():
+    def call_telemetry_report(self):
+        """Example xml - https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/GetSampleStatus.cgi?rettype=xml&study_id=phs000452"""
         response = requests.get(
             self.endpoint, 
             headers={"Content-Type": "application/json"}
         )
         status_code = response.status_code
 
-        return response.text
+        return xmltodict.parse(response.text)
 
-    def _is_bioproject_admin(xml_object):
-        return xml_object.attrib['bp_type'] == 'admin'
-        
-    def _is_sample(xml_object):
-        return xml_object.attrib['submitted_sample_id'] == self.alias
+    def get_sample_status(self, alias, data_type):
+        try:
+            telemetry_data = self.call_telemetry_report()
 
-    def extract_sample_level_info(alias):
-        root = ET.fromstring(call_telemetry_report())
-        samples = [x.attrib for x in root.iter('Sample') if self._is_sample(x)]
-        bio_projects = [x.attrib['bp_id'] for x in root.iter('BioProject') if self._is_bioproject_admin(x)]
+            study_data = telemetry_data["DbGap"]["Study"]
+            sample_list = study_data["SampleList"]["Sample"]
 
-        if not samples:
-            raise SampleNotRegisteredException('Sample not registered with Dbgap')
-        if not bio_projects:
-            raise StudyNotRegisteredException('Study not registered with Dbgap')
-        if len(samples) > 1:
-            raise Exception('Could not find specific sample in report')
-        
-        return {
-            "study": root[0].attrib,
-            "bio_project": bio_projects[0],
-            "dbgap_sample_info": samples[0]
-        }
+            for sample in sample_list:
+                if sample.get("@submitted_sample_id") == alias:
+                    sra_data = sample["SRAData"]
+                    sra_sample_stats = sra_data["Stats"]
+
+                    if isinstance(sra_sample_stats, list):
+                        for stat in sra_sample_stats:
+                            if stat.get("@experiment_type") == data_type:
+                                return stat["@status"]
+                    elif isinstance(sra_sample_stats, dict):
+                        return sra_sample_stats["@status"]
+            
+            raise SampleNotFoundError(alias)
+ 
+        except KeyError as e:
+            print(f"Error: {e}")
+            # Re-raise the exception so we force the wdl to fail
+            raise e
