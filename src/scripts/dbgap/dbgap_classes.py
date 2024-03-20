e3d3c0c6cf4924c39f728284de60bad46a2384e5
phendriksen100 <103142505+phendriksen100@users.noreply.github.com>
2024-03-20 11:07:12 -0400

    remove print

--- src/scripts/dbgap/dbgap_classes.py
+++ src/scripts/dbgap/dbgap_classes.py
@@ -111,7 +111,6 @@ class ReadGroup:
         # 2. It can have a dict with keys itemsType , and items. This happens when the metadata is empty, but not for other samples
         # 3. Just has the metadata
         if "submission_metadata" in first_read_group and "items" not in first_read_group["submission_metadata"]:
-            print("vlaue", first_read_group.get("items"))
             # Replace single quotes with double quotes to make it valid JSON
             submission_metadata_str = first_read_group["submission_metadata"].replace("'", "\"")
 
